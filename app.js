7f77822733ce4b151dd6edb22c71aaa434787ee5
doniwah <whyddoni@gmail.com>
2025-02-19 23:58:51 +0700

    update

--- app.js
+++ app.js
@@ -1,27 +1,27 @@
-const express = require('express')
-const expressLayouts = require('express-ejs-layouts')
-const mysql = require("mysql")
-const app = express()
-const port = 8000
+const express = require("express");
+const expressLayouts = require("express-ejs-layouts");
+const mysql = require("mysql");
+const app = express();
+const port = 8000;
 
-app.set('view engine', 'ejs')
-app.use(expressLayouts)
+app.set("view engine", "ejs");
+app.use(expressLayouts);
 
 //aplication middleware
-app.use(express.static('public'))
+app.use(express.static("public"));
 
 //db
 const db = mysql.createConnection({
-    host: "localhost",
-    database: "hmj",
-    user: "root",
-    password: ""
-})
+  host: "localhost",
+  database: "hmj",
+  user: "root",
+  password: "",
+});
 
 db.connect((err) => {
-    if (err) throw err
-    console.log('Database connected')
-})
+  if (err) throw err;
+  console.log("Database connected");
+});
 
 const sql = `
     SELECT 
@@ -34,53 +34,52 @@ const sql = `
     ORDER BY tanggal ASC
 `;
 
-app.get('/', (req, res) => {
-    db.query(sql, (err, result) => {
-        if (err) throw err;
-        
-        // Process the results
-        const events = result.map(item => ({
-            ...item,
-            base64Image: item.base64Image ? `data:image/jpeg;base64,${item.base64Image}` : null,
-            dateDisplay: `${item.formatted_date}\n${item.tahun}`
-        }));
-        
-        // Render the page with all required data
-        res.render("index", {
-            layout: 'layouts/main-layout',
-            css: '/css/style.css',
-            js: '/js/script.js',
-            events: events,
-            title: "HMJ TI 2025"
-        });
+app.get("/", (req, res) => {
+  db.query(sql, (err, result) => {
+    if (err) throw err;
+
+    // Process the results
+    const events = result.map((item) => ({
+      ...item,
+      base64Image: item.base64Image ? `data:image/jpeg;base64,${item.base64Image}` : null,
+      dateDisplay: `${item.formatted_date}\n${item.tahun}`,
+    }));
+
+    // Render the page with all required data
+    res.render("index", {
+      layout: "layouts/main-layout",
+      css: "/css/style.css",
+      js: "/js/script.js",
+      events: events,
+      title: "HMJ TI 2025",
     });
+  });
 });
 
-app.get('/about', (req, res) => {
-    res.render('about', {
-        title: 'Halaman About',
-        layout: 'layouts/main-layout',
-        css: '/css/about.css',
-        js: '/js/script.js'
-    })
-})
+app.get("/about", (req, res) => {
+  res.render("about", {
+    title: "Halaman About",
+    layout: "layouts/main-layout",
+    css: "/css/about.css",
+    js: "/js/script.js",
+  });
+});
 
-app.get('/management', (req, res) => {
-    res.render('management', {
-        title: 'Halaman management',
-        layout: 'layouts/main-layout',
-        css: '/css/management.css',
-        js: '/js/management.js'
-    })
-})
-
+app.get("/management", (req, res) => {
+  res.render("management", {
+    title: "Halaman management",
+    layout: "layouts/main-layout",
+    css: "/css/management.css",
+    js: "/js/management.js",
+  });
+});
 
 // const sql = `
-//         SELECT 
-//             artikel.*, 
-//             kategori.nama_kategori, 
-//             TO_BASE64(picture) as base64Image 
-//         FROM artikel 
+//         SELECT
+//             artikel.*,
+//             kategori.nama_kategori,
+//             TO_BASE64(picture) as base64Image
+//         FROM artikel
 //         JOIN kategori ON artikel.id_kategori = kategori.id_kategori
 //     `
 
@@ -111,10 +110,10 @@ app.get('/management', (req, res) => {
 // })
 
 app.use((req, res) => {
-    res.status(404)
-    res.send('404')
-})
+  res.status(404);
+  res.send("404");
+});
 
 app.listen(port, () => {
-    console.log("berhasil connect");
-})
+  console.log("berhasil connect");
+});
